653fd2c8a98d9c90a645deae075b1eea07c95571
chemhunter <science.hao@gmail.com>
2025-09-15 00:14:22 +0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -137,7 +137,7 @@ async function fetchAITimestamps(subtitlesText, commentText ='') {
     2. 不要回复任何JSON对象之外的额外文字、解释或注释。
     3. 如果在字幕中找到明确的商业推广，请填写 "start", "end", "product" 字段，并将 "noAd" 设为 false。
     4. 如果在仔细分析后，确定字幕中【没有】任何商业推广，返回{"start": null, "end": null, "product": null, "noAd": true}。
-    5. 商业广告一般不低于30s，将与主题无关的、引入广告的先导部分也视做广告。
+    5. 商业广告一般不低于30s，将偏离视频主题、有意引入广告的先导部分也视做广告区间。
     6. 将最后一条广告字幕接下来的下一条正常字幕的时间减去1s作为"end"时间戳。
 `
   const user_prompt = `
